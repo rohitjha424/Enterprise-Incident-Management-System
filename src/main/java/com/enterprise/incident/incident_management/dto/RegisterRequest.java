106c262ab04bcfbb3dcd9f36b6eb8774ec5c7385
rohitjha424 <rohitjha424@gmail.com>
2026-02-25 23:31:08 +0530

    Formatting

--- src/main/java/com/enterprise/incident/incident_management/dto/RegisterRequest.java
+++ src/main/java/com/enterprise/incident/incident_management/dto/RegisterRequest.java
@@ -1,20 +1,23 @@
 package com.enterprise.incident.incident_management.dto;
 
 public class RegisterRequest {
-    private String username;
-    private String password;
+	private String username;
+	private String password;
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
-    
 }
