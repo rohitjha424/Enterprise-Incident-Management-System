03635c2325e25081633e965ff9748798e2e55e9d
rohitjha424 <rohitjha424@gmail.com>
2026-02-28 00:17:58 +0530

    Implementing Redis

--- src/main/java/com/enterprise/incident/incident_management/Application.java
+++ src/main/java/com/enterprise/incident/incident_management/Application.java
@@ -2,7 +2,9 @@ package com.enterprise.incident.incident_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class Application {
 
