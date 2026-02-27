0397ef1228e34b4e1dd6b58d03df207900ee1071
rohitjha424 <rohitjha424@gmail.com>
2026-02-27 22:23:53 +0530

    Incident  Event Class Added

---
+++ src/main/java/com/enterprise/incident/incident_management/event/IncidentCreatedEvent.java
@@ -0,0 +1,73 @@
+package com.enterprise.incident.incident_management.event;
+
+
+
+import java.time.LocalDateTime;
+
+public class IncidentCreatedEvent {
+
+    private Long id;
+    private String title;
+    private String priority;
+    private String reportedBy;
+    private LocalDateTime createdAt;
+    
+    
+    
+	public IncidentCreatedEvent(Long id, String title, String priority, String reportedBy, LocalDateTime createdAt) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.priority = priority;
+		this.reportedBy = reportedBy;
+		this.createdAt = createdAt;
+	}
+	
+	
+	public IncidentCreatedEvent() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getPriority() {
+		return priority;
+	}
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+	public String getReportedBy() {
+		return reportedBy;
+	}
+	public void setReportedBy(String reportedBy) {
+		this.reportedBy = reportedBy;
+	}
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+
+	@Override
+	public String toString() {
+		return "IncidentCreatedEvent [id=" + id + ", title=" + title + ", priority=" + priority + ", reportedBy="
+				+ reportedBy + ", createdAt=" + createdAt + "]";
+	}
+
+}
